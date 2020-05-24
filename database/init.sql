43ef0dc7442e1d76adab7e060cf12a5f45232d85
mac <mac@macs-MBP.fios-router.home>
2020-05-23 20:50:18 -0400

    add

--- database/init.sql
+++ database/init.sql
@@ -6,8 +6,8 @@ ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
 flush privileges;
 
 CREATE TABLE my_data(
-     plate VARCHAR(100),
-     time_stamp DATETIME,
-     lat float,
-     lon float
+     name VARCHAR(100)
      );
+
+INSERT INTO my_data(name)
+VALUES('sam');
